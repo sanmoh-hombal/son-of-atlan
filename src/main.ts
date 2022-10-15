35c20ca7cdf73b74fe2937fc24c8b603db06d39b
Sanmoh Hombal <sanmoh.hombal@gmail.com>
2022-10-15 22:42:07 +0530

    ✨ feat(ui-library): antd init
    Boilerplating AntD and all necessary configs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp, type App as VueApp } from "vue";
 
+import Antd from "ant-design-vue";
+import "ant-design-vue/es/message/style/css";
+
 import App from "@/App.vue";
 import router from "@router";
 
 const app: VueApp<Element> = createApp(App);
 
 app.use(router);
+app.use(Antd);
 
 app.mount("#app");
