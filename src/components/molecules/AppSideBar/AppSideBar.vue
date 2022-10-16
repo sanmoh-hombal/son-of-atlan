418c36b608689697b3a95d313dd122de557c1656
Sanmoh Hombal <sanmoh.hombal@gmail.com>
2022-10-16 11:33:31 +0530

    ✨ feat(component): dynamic sidebar
    Loading States, Dyanmic Routes and Programmatic Redirects

--- src/components/molecules/AppSideBar/AppSideBar.vue
+++ src/components/molecules/AppSideBar/AppSideBar.vue
@@ -1,22 +1,50 @@
 <script setup lang="ts">
+import { computed } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
 import { BorderlessTableOutlined, HomeOutlined } from "@ant-design/icons-vue";
+
+import { useTablesStore } from "@store";
+
+const route = useRoute();
+const router = useRouter();
+const tablesStore = useTablesStore();
+
+const activeItem = computed((): string[] => {
+  if (route.name === "home") return ["home"];
+  if (route.name === "explorer" && route.params.name) return [route.params.name.toString()];
+
+  return [];
+});
+
+/** Redirect using key and keyPaths ingested from a-menu @click event */
+function redirect({ key, keyPath }: { key: string; keyPath: string[] }) {
+  if (key === "home") router.push("/");
+  else router.push(`/${keyPath.join("/")}`);
+}
 </script>
 
 <template>
-  <a-menu mode="inline" style="height: 100%">
+  <a-menu :selected-keys="activeItem" mode="inline" :open-keys="['explorer']" style="height: 100%" @click="redirect">
     <a-menu-item key="home">
       <template #icon>
         <home-outlined />
       </template>
       Home
     </a-menu-item>
-    <a-sub-menu>
+    <a-sub-menu key="explorer">
       <template #icon>
         <borderless-table-outlined />
       </template>
       <template #title>Explorer</template>
-      <a-menu-item key="categories">categories</a-menu-item>
-      <a-menu-item key="orders">orders</a-menu-item>
+      <div v-if="tablesStore.loading" style="text-align: center">
+        <a-spin />
+      </div>
+      <template v-else>
+        <a-menu-item v-for="table in tablesStore.tables" :key="table.name">
+          {{ table.name }}
+        </a-menu-item>
+      </template>
     </a-sub-menu>
   </a-menu>
 </template>
